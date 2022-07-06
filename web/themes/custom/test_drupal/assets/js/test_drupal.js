117776e9cc54db2b78ba6af48f3d97c54a37089a
Dhim7 <allali.mehdi.ar@gmail.com>
2022-07-06 17:25:24 +0200

    Add custom block and change base theme.

--- web/themes/custom/test_drupal/assets/js/test_drupal.js
+++ web/themes/custom/test_drupal/assets/js/test_drupal.js
@@ -7,11 +7,27 @@
   const testDrupal = {
     main: function () {
       testDrupal.agenda();
+      testDrupal.relatedBlock();
     },
     agenda: function () {
       if ($('.view-id-agenda .button').length) {
         testDrupal.selectContainer();
       }
+    },
+    relatedBlock: function () {
+      if ($('.block-related-event-block .event-link').length) {
+        testDrupal.manageOverlay();
+      }
+    },
+    manageOverlay: function () {
+      let content = $('.block-related-event-block .event-link');
+      content.hide();
+      $(".block-related-event-block h3").click(function(){
+        content.show();
+      });
+      $(".block-related-event-block .event-link svg").click(function(){
+        content.hide();
+      });
     },
     selectContainer: function () {
       $( ".views-col .button button" ).on( "click", function() {
